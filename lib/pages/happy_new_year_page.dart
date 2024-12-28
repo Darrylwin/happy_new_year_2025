2dc0c13c86d63c860b2d01f13b22a09504768368
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-12-28 23:37:55 +0000

    happy_new_year widget

--- lib/pages/happy_new_year_page.dart
+++ lib/pages/happy_new_year_page.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
-class HappyNewYearPage extends StatelessWidget {
+class HappyNewYearPage extends StatefulWidget {
   const HappyNewYearPage({super.key});
 
   @override
+  State<HappyNewYearPage> createState() => _HappyNewYearPageState();
+}
+
+class _HappyNewYearPageState extends State<HappyNewYearPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 2, 21, 36),
+                  Color.fromARGB(255, 1, 15, 25),
+                  Color.fromARGB(255, 0, 10, 20),
+                  Color.fromARGB(255, 1, 15, 25),
+                  Color.fromARGB(255, 2, 21, 36),
+                ],
+                stops: [0.0, 0.3, 0.5, 0.7, 1.0],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          
+        ],
+      ),
+    );
   }
 }
