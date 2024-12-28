c6414853cde7a8d0c30d3dfd81158acefaac3489
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-12-28 23:05:47 +0000

    first commit

---
+++ lib/pages/happy_new_year_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HappyNewYearPage extends StatelessWidget {
+  const HappyNewYearPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
